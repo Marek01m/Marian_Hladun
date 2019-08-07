3d702e0563f2118ffdd07c81a32891d6b75ef305
Marian Hladun <mgladun777@gmail.com>
2019-08-07 23:00:15 +0300

    added task 1\2 les 1

---
+++ Lesson 1/Task 1.py
@@ -0,0 +1,8 @@
+a=5
+b=2
+print("a=", a)
+print("b=", b)
+print("a+b=", a+b)
+print("a-b=", a-b)
+print("a*b=", a*b)
+print("a/b=", a/b)
---
+++ Lesson 1/Task 2.py
@@ -0,0 +1,4 @@
+name=input("Whats ur name?\n")
+age=input("How old are u?\n")
+city=input("Where do u live(city)?\n")
+print("Hello,{}! Your name is {}, you live in {}.".format(name,age,city))
