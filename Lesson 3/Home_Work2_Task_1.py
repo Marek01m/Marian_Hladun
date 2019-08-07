52bcc2ad8a3cb0dbe1d5d6ebea430ae990f2fe25
Marian Hladun <mgladun777@gmail.com>
2019-08-07 20:08:55 +0300

    added some lines

--- Lesson 3/Home_Work2_Task_1.py
+++ Lesson 3/Home_Work2_Task_1.py
@@ -17,7 +17,12 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!'''
+print("________________________________________________________________________")
 print('better quantity--->',zen.count('better'))
 print('never quantity--->',zen.count('never'))
 print('is quantity--->',zen.count('is'))
+print("________________________________________________________________________")
+print(zen.upper())
+print("________________________________________________________________________")
 print(zen.replace("i","&"))
+print("________________________________________________________________________")
