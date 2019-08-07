be20dae230976011128e3389737663da25562102
Marian Hladun <mgladun777@gmail.com>
2019-08-07 21:53:12 +0300

    second task completed

---
+++ Lesson 3/Home_Work2_Task_2.py
@@ -0,0 +1,12 @@
+number=input("Введіть чотирьохзначне число: ")
+n1=int(number[0])
+n2=int(number[1])
+n3=int(number[2])
+n4=int(number[3])
+print('Добуток: ', n1*n2*n3*n4)
+
+reverse_num='{3}{2}{1}{0}'.format(n1,n2,n3,n4)
+print('Реверсний порядок: ',reverse_num)
+
+#sort_list=[n1,n2,n3,n4]
+print("Посортований масив в порядку зростання: ",sorted(number))
