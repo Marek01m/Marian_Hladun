c0ac07ecdc7a8eb65484f3988b8d0f6c6d0e2216
Marian Hladun <mhladtc@training.local>
2019-08-08 18:00:10 +0300

    classwrk

---
+++ Lesson 4/Classwork.py
@@ -0,0 +1,82 @@
+# n=0
+# finish=100
+# while n<finish:
+#     if n%2==0:
+#         print(n)
+#     n+=1    
+#     continue
+     
+# print("________________________")
+
+
+# for i in range(100):
+#     if i%2==0
+#     print(i)
+
+# print("________________________")
+
+# for i in range(0,100,2)
+#     print(i)
+
+# print("________________________")
+
+# print(list(range(0,100,2)))
+
+# print("________________________")
+#task2
+
+#print(list(range(1,100,2)))
+
+# i=0
+# while i<100:
+#     if i%2>0:
+#         print(i)
+#     i+=1    
+#     continue
+####################
+# for item in range(100)
+#     if item%2==0
+#         continue
+#     print(item) 
+# 
+#for item in range(1,100,2)
+#    print(item) 
+#    
+#print(list(range(1,100,2)))
+######################################
+
+# print("________________________")
+#task3
+
+# numlist=list(range(0,101))   #m
+# i=0
+# while numlist[i]%2>0:
+#     i+=1
+#     break
+# print("список містить непарне число")    
+
+# list_number=[2,4,6,8,9,10]    #chck                        
+# contain_odd=False
+# for item in list_number:
+#     if not item % 2 ==0:
+#         contain_odd=True
+#         break
+# if contain_odd:
+#     print("There is odd number in the list")
+# else:
+#     print("There is only even number in the list")            
+
+
+#
+# print("________________________")
+#task4
+
+# numlist=list(range(0,101))  #dorobity
+# for item in numlist:  
+#     float(item)
+#     print(item)
+
+# numlist=list(range(10))      #y
+# while i<10:
+#     float(a[i])
+#     i+=1
