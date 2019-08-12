398f4c89ef8d419d8e42ea6096b645ea8c58618e
Marian Hladun <mhladtc@training.local>
2019-08-12 18:00:21 +0300

    new les

---
+++ Lesson 5/Classwork.py
@@ -0,0 +1,45 @@
+# nums=[]
+# k=int(input("Яка довжина списку "))
+# #i=0
+# # while i<length_list:
+# #     numlist[i]=input("Введіть елемент:")
+# #     i+=1
+# #     print(numlist[i])
+  
+
+# for i in range(k):
+#     n=int(input("Enter the element"))
+#     nums.append(n)
+
+
+
+
+##########################################
+#task 2
+
+# numlist=list(range(1,11))
+# print(numlist)
+# parni2=[]
+# neparni3=[]
+# ni2ni3=[]
+# for i in numlist:
+#     if i%2==0:
+#         parni2.append(i)
+#     elif i%3==0:
+#         neparni3.append(i)
+#     elif i%2>0 and i%3>0:
+#         ni2ni3.append(i)
+# print(parni2)
+# print(neparni3)
+# print(ni2ni3)
+
+#######################
+#task 3
+
+n=int(input("Enter num "))
+n_list=list(range(1,n+1))
+k=1
+
+for i in n_list:
+    k*=i
+print("!k=",k)
