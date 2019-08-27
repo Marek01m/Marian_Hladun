7bad44acce95eb4e40c8921831177f9e308ccd02
Marian Hladun <mhladtc@training.local>
2019-08-27 18:01:44 +0300

    smtg new

---
+++ Lesson 9/Classwork9.py
@@ -0,0 +1,26 @@
+# class A:    pass
+# def myMethod (self, x):    return x * x
+# A.m1 = myMethod
+# A.attr1 = 2 * 2
+
+
+# class Animal:
+#     def __init__(self,type_a,sound,color):
+#         self.t=type_a
+#         self.s=sound
+#         self.c=color
+    
+#     def animal_info(self):
+#         print("Animal type is {}, its sound is {}, its color is {}".format(self.t,self.s,self.c))
+
+# an=Animal("cow","mooo","black n white")
+
+# an.animal_info()
+
+
+# class Figure:
+#     def __init__(self,color):
+#         self.c=color
+    
+#     def get_color(self):
+#         print self.c
