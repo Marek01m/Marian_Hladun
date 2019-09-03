e332292ac6072eda77fef02096821c58b3ab22af
Marian Hladun <mgladun777@gmail.com>
2019-09-03 20:38:50 +0300

    smtg new

--- Lesson 9/Classwork9.py
+++ Lesson 9/Classwork9.py
@@ -62,3 +62,37 @@ print(f"Area is {r.square()}")
 sq=Square(5)
 sq.info()
 print(f"Area is {sq.square()}")            
+
+#exmplsecnd____________________________________________________________________
+class Figure:
+    def __init__(self,color):
+        self.color=color
+                
+    def get_color(self):
+        return self.color
+
+    def info(self):
+        print("Figure")
+        print("Color: " + self.color)    
+
+class Rectangle(Figure):
+    def __init__(self, color, width=100, height=100):
+        super().__init__(color)
+        self.width=width
+        self.height=height
+            
+    def square(self):
+        return self.width*self.height
+
+    def info(self):
+        print("Rectangle")
+        print("Color: "+ self.color)
+        print("Width: "+str(self.width)) 
+        print("Height: "+str(self.height))
+        print("Square: "+str(self.square()))
+
+fig1=Figure("green")
+fig1.info()
+fig2=Rectangle("red",81,62)
+fig2.info()        
+
